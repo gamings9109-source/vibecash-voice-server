4fb8658db02a32f0b23980919de82b206b8fd982
Sahilfragzyt <gamings9109@gmail.com>
2026-07-04 12:02:01 +0530

    Add server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("VibeCash Voice Server Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
